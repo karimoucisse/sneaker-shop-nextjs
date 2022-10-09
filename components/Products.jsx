2c3a9662d98afe3656dd72778b33d3d4547e21ef
Karimou Cisse <karimou.cisse1@gmail.com>
2022-10-09 15:34:03 +0200

    fixed products filter

--- components/Products.jsx
+++ components/Products.jsx
@@ -6,23 +6,23 @@ import { useEffect, useState } from "react"
 const Products = ({products}) => {
   const filters = useSelector((state) => state.filter)
   const [filteredProducts, setFilteredProducts] = useState()
+  const [sortProducts, setSortProducts] = useState()
 
   useEffect(() => {
-    setFilteredProducts(
-      products.filter((product) => 
-        product.brand.includes(filters.filter)
-      )
-    ) 
-  },[filters, products])
+    // if the filters.filter is == "Toutes les chaussures", then print all products,
+    // else print product that correspond to the filter
+      setFilteredProducts(
+        filters.filter !== "Toutes les chaussures" ?
+        products.filter((product) => 
+          product.brand.includes(filters.filter)
+        ) : products
+      ) 
 
-  if(!filteredProducts) {
-    return null
-  }
+  },[])
 
   return (
     <div className= {styles.container}>
-        {filteredProducts.map(product => {
-          console.log(product);
+        {filteredProducts?.map(product => {
           return <ProductCard key= {product._id} product= {product}/>
         })}
     </div>
